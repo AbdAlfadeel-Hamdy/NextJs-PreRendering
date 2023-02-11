9ff4f67c5ffaf75010d299c5232ef10628df378a
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-11 08:10:43 +0200

    getServerSideProps

---
+++ pages/user-profile.js
@@ -0,0 +1,13 @@
+const UserProfilePage = (props) => {
+  return <h1>{props.userName}</h1>;
+};
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      userName: "Max",
+    },
+  };
+}
